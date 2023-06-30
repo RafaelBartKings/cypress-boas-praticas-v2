bfe12914bb89d4f6f147335a0065cce8094c5511
Rafael Reis <rafyangtj@gmail.com>
2023-06-30 14:16:14 -0300

    wrong abstraction

--- cypress/e2e/wrong-abstraction/sample.cy.js
+++ cypress/e2e/wrong-abstraction/sample.cy.js
@@ -1,32 +1,29 @@
 describe('Wrong abstraction bad practice', () => {
-  beforeEach(() => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
+   beforeEach(() => {
+      cy.intercept('GET', '**/search**').as('getStories')
 
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-  })
+      cy.visit('https://hackernews-seven.vercel.app')
+      cy.wait('@getStories')
+   })
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('cypress')
-    cy.wait('@getStories')
+   it('uses custom command for assertion just for the sake of reusability', () => {
+      cy.search('cypress')
+      cy.wait('@getStories')
 
-    cy.assertResults()
-  })
+      cy.get('.table-row').its('length').should('be.at.least', 1)
+   });
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('selenium')
-    cy.wait('@getStories')
+   it('uses custom command for assertion just for the sake of reusability', () => {
+      cy.search('selenium')
+      cy.wait('@getStories')
 
-    cy.assertResults()
-  })
+      cy.get('.table-row').its('length').should('be.at.least', 1)
+   });
 
-  it('uses custom command for assertion just for the sake of reusability', () => {
-    cy.search('playwright')
-    cy.wait('@getStories')
+   it('uses custom command for assertion just for the sake of reusability', () => {
+      cy.search('playwright')
+      cy.wait('@getStories')
 
-    cy.assertResults()
-  })
+      cy.get('.table-row').its('length').should('be.at.least', 1)
+   });
 })
